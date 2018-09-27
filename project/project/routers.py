151280e02f51f9ffb3c57b465c54ad5c7a9aa2cc
Daniel Vera <daveralo@gmail.com>
2018-09-26 22:38:16 -0500

    vue detail and views deatil

--- project/project/routers.py
+++ project/project/routers.py
@@ -4,3 +4,6 @@ from risk.viewsets import RiskViewSet, FieldViewSet
 router = routers.DefaultRouter()
 router.register(r'risk', RiskViewSet)
 router.register(r'field', FieldViewSet)
+#router.register(r'risk/(?P<risk_id>\d+)/fields', FieldViewSet)
+#router.register(r'risk/(?P<risk_id>\d+)/fields', FieldViewSet, base_name = 'land_list')
+
--- project/risk/viewsets.py
+++ project/risk/viewsets.py
@@ -1,12 +1,21 @@
 from rest_framework import viewsets
 from .models import Risk, Field
 from .serializers import RiskSerializer, FieldSerializer
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 
 class RiskViewSet(viewsets.ModelViewSet):
     queryset = Risk.objects.all()
     serializer_class = RiskSerializer
 
-class FieldViewSet(viewsets.ModelViewSet):
-    queryset = Field.objects.all()
-    serializer_class = FieldSerializer   
+    @detail_route()
+    def field_list(self, request, pk=None):
+        risk = self.get_object()
+        fields = Field.objects.filter(risk=risk)
+        fields_json = FieldSerializer(fields, many=True)
+        return Response(fields_json.data)
 
+
+class FieldViewSet(viewsets.ModelViewSet):
+    queryset = Field.objects.all() 
+    serializer_class = FieldSerializer  
