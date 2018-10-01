ce4373471ec3e2ac275066bbe29093086a608e4b
Daniel Vera <daveralo@gmail.com>
2018-09-30 22:28:15 -0500

    ajustes test

--- project/risk/tests/test_models.py
+++ project/risk/tests/test_models.py
@@ -1,10 +1,9 @@
 import json
 from django.test import RequestFactory, TestCase
-from .models import Risk, Field
-from rest_framework.test import APIClient
+from risk.models import Risk, Field
+from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
 from django.urls import reverse
-from . import viewsets
 
 
 def response_to_json(response):
@@ -27,22 +26,5 @@ class ModelTestCase(TestCase):
         new_count = Risk.objects.count()
         self.assertNotEqual(old_count, new_count)
 
-class ViewTestCase(TestCase):
-    """Test suite for the api views."""
-    def setUp(self):
-        self.factory = RequestFactory()    
 
-    #def test_should_return_empty_array(self):
-        # when
-        #request = self.factory.get('/api/risk/')
-        #print(request)
-        #response = viewsets.risk_list(request)
-        #print(response)
-        #json_content = response_to_json(response)
 
-        # then
-        #self.assertEqual(len(json_content), 0)    
-
-    def test_view_url_exists_at_desired_location(self):
-        response = self.client.get('/api/risk/')
-        self.assertEqual(response.status_code, 200)    
