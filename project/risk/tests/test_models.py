3bd5c3aa1a943fee8d2f9a8ccf6bfa2b3b6002e3
Daniel Vera <daveralo@gmail.com>
2018-10-01 22:00:02 -0500

    test

--- project/risk/tests/test_models.py
+++ project/risk/tests/test_models.py
@@ -1,20 +1,11 @@
-import json
 from django.test import RequestFactory, TestCase
 from risk.models import Risk, Field
-from rest_framework.test import APIClient, APITestCase
-from rest_framework import status
-from django.urls import reverse
 
-
-def response_to_json(response):
-    str_content = response.content.decode("utf-8")
-    return json.loads(str_content)
 
 class ModelTestCase(TestCase):
-    """This class defines the test suite for the risk model."""
 
     def setUp(self):
-        """Define the test client and other test variables."""
+        """Define test variables."""
         self.name = "risk_test"
         self.description = "description for the risk"
         self.risk = Risk(name=self.name, description=self.description)
@@ -25,6 +16,3 @@ class ModelTestCase(TestCase):
         self.risk.save()
         new_count = Risk.objects.count()
         self.assertNotEqual(old_count, new_count)
-
-
-
--- project/risk/tests/tests_views.py
+++ project/risk/tests/tests_views.py
@@ -1,47 +1,18 @@
-#import json
-#from django.test import RequestFactory, TestCase
 from risk.models import Risk, Field
 from rest_framework.test import APITestCase
-#from rest_framework import status
-#from django.urls import reverse
-from django.shortcuts import reverse
-#from risk import viewsets
-
-
 
 class ViewTestCase(APITestCase):
     def setUp(self):
-        #self.factory = RequestFactory()    
         self.name = "risk_test"
         self.description = "description for the risk"
         self.risk = Risk(name=self.name, description=self.description)
-
-    #def test_should_return_empty_array(self):
-        # when
-        #request = self.factory.get('/api/risk/')
-        #print(request)
-        #response = viewsets.risk_list(request)
-        #print(response)
-        #json_content = response_to_json(response)
-
-        # then
-        #self.assertEqual(len(json_content), 0)    
+        self.risk.save()
 
     def test_view_url_exists(self):
         response = self.client.get('/api/risk/')
         self.assertEqual(response.status_code, 200)   
 
-    def test_risk_creation(self):
-        response = self.client.post(reverse('risk'), {
-            'name': 'automobile',
-            'description': 'Risk for automobile insurance'
-        })
-
-        # assert new movie was added
-        self.assertEqual(Risk.objects.count(), 2)
-
-        # assert a created status code was returned
-        self.assertEqual(201, response.status_code)        
-
-
+    def test_getting_risk(self):
+        response = self.client.get('/api/risk/', format="json")
+        self.assertEqual(len(response.data), 1)             
 
